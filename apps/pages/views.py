3aead59e4673d81c57623b09fa86f6b9d3dbc1a3
eticialima <leticialimacgi@gmail.com>
2024-11-03 15:57:12 -0300

    Passo 10 - Mensagens de Alerta

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    messages.success(request, "Operação realizada com sucesso!")
+    messages.error(request, "Ocorreu um erro!")
+    messages.warning(request, "Atenção com esta ação.")
+    messages.info(request, "Essa é uma informação.")
     return render(request, 'index.html')
--- core/settings.py
+++ core/settings.py
@@ -233,3 +233,15 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+
+# Configuração de Messages (Class Boostrap) #
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
