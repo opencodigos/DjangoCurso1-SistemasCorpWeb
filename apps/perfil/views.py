f6d5ae6629b9afa21fefa3d6c52227ea3bbb507b
eticialima <leticialimacgi@gmail.com>
2024-11-04 11:48:47 -0300

    Passo 55 - Paginação - Lista de Postagens (Perfil)

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import get_object_or_404, render
 from contas.models import MyUser
 from forum.forms import PostagemForumForm
+from django.core.paginator import Paginator
+
 
 def perfil_view(request, username):
     modelo = MyUser.objects.select_related('perfil').prefetch_related('user_postagem_forum')
     perfil = get_object_or_404(modelo, username=username)
-
     form_dict = {}
     for el in perfil.user_postagem_forum.all():
         form = PostagemForumForm(instance=el) 
         form_dict[el] = form
-
-    return render(request, 'perfil.html', {'obj': perfil,'form_dict':form_dict})
+    
+    # Criar uma lista de tuplas (postagem, form) a partir do form_dict
+    form_list = [(postagem, form) for postagem, form in form_dict.items()]
+    
+    # Aplicar a paginação à lista de tuplas
+    paginacao = Paginator(form_list, 3)
+    
+    # Obter o número da página a partir dos parâmetros da URL
+    pagina_numero = request.GET.get("page")
+    page_obj = paginacao.get_page(pagina_numero)
+    
+    # Criar um novo dicionário form_dict com base na página atual
+    form_dict = {postagem: form for postagem, form in page_obj}
+    context = {'obj': perfil, 'page_obj': page_obj, 'form_dict':form_dict}
+    return render(request, 'perfil.html', context)
