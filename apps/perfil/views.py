6337736c0883343bc466f1c8dc3c3fd2ba709e05
eticialima <leticialimacgi@gmail.com>
2024-11-04 01:24:48 -0300

    Passo 41 - Rota Lista de Postagens (Perfil do Usuário)

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from contas.models import MyUser
 
 def perfil_view(request, username):
-    perfil = get_object_or_404(MyUser.objects.select_related('perfil'), username=username)
+    modelo = MyUser.objects.select_related('perfil').prefetch_related('user_postagem_forum')
+    perfil = get_object_or_404(modelo, username=username)
     context = {'obj': perfil}
     return render(request, 'perfil.html', context)
