1c917b684ed6c1f2b6c1edee48a095eb5c0515ad
eticialima <leticialimacgi@gmail.com>
2024-11-04 15:55:53 -0300

    Passo 73 - Modelo

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.site.register(Pagina)
+admin.site.register(TipoBloco)
+admin.site.register(Conteudo)
+admin.site.register(Blocos)
+
