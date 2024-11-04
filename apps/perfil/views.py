bbc3802c551fd830cb1b99132e89a0b95f618719
eticialima <leticialimacgi@gmail.com>
2024-11-04 09:41:32 -0300

    Passo 45 - Editar Postagem (Modal) - Rota Perfil do Usuário

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from contas.models import MyUser
+from forum.forms import PostagemForumForm
 
 def perfil_view(request, username):
     modelo = MyUser.objects.select_related('perfil').prefetch_related('user_postagem_forum')
     perfil = get_object_or_404(modelo, username=username)
-    context = {'obj': perfil}
-    return render(request, 'perfil.html', context)
+
+    form_dict = {}
+    for el in perfil.user_postagem_forum.all():
+        form = PostagemForumForm(instance=el) 
+        form_dict[el] = form
+
+    return render(request, 'perfil.html', {'obj': perfil,'form_dict':form_dict})
